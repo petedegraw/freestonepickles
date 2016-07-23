8119cb79123a380c2ed613dd73d7f17c06dd1fa9
Pete DeGraw <petedegraw@gmail.com>
2016-07-22 22:15:00 -0600

    updates viewport size

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head profile="http://gmpg.org/xfn/11">
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	
 	<?php if (is_search()) { ?>
